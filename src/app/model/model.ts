e8ef69fa11360f2220c5843a4a4eca6c4c22817a
Marcus Borges <yahuchanam@gmail.com>
2024-05-14 10:42:06 -0300

    feat: add card booster models

--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -30,3 +30,45 @@ export type CollectionSetPayload = {
 export type CollectionSet = Omit<CollectionSetPayload, 'releaseDate'> & {
   releaseDate: Date;
 };
+
+export type BoosterPayload = {
+  cards: CardBooster[];
+};
+
+export type CardBoosterForeignName = {
+  name: string;
+  text: string;
+  type: string;
+  flavor: string;
+  imageUrl: string;
+  language: string;
+  multiverseid: number;
+};
+
+export type CardsBoosterLegality = {
+  format: string;
+  legality: string;
+};
+
+export type CardBooster = {
+  id: string;
+  name: string;
+  cmc: number;
+  type: string;
+  types: string[];
+  rarity: string;
+  set: string;
+  setName: string;
+  text: string;
+  flavor: string;
+  artist: string;
+  number: string;
+  layout: string;
+  multiverseid: string;
+  imageUrl: string;
+  foreignNames: CardBoosterForeignName[];
+  printings: string[];
+  originalText: string;
+  originalType: string;
+  legalities: CardsBoosterLegality[];
+};
