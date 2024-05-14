daacbb70e9ec663ace5acac4951209cb5fd1fae0
Marcus Borges <yahuchanam@gmail.com>
2024-05-14 10:12:16 -0300

    feat: add open-deck link on collection set component

--- src/app/pages/card-collections/collection-set/collection-set.component.ts
+++ src/app/pages/card-collections/collection-set/collection-set.component.ts
@@ -1,11 +1,13 @@
 import { Component, input } from '@angular/core';
 import { CollectionSet } from '../../../model/model';
 import { DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-collection-set',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, RouterModule, MatButtonModule],
   templateUrl: './collection-set.component.html',
   styleUrl: './collection-set.component.scss',
 })
