91609ec4cf86b407f00de8cd0f68b9b1c5fa909c
Marcus Borges <yahuchanam@gmail.com>
2024-05-14 11:08:00 -0300

    feat: add recursive retrive creatures deck

--- src/app/services/booster/booster.service.ts
+++ src/app/services/booster/booster.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpDefaultService } from '../http-default/http-default.service';
+import { Observable, filter, map, of, switchMap, tap } from 'rxjs';
+import { BoosterPayload, CardBooster } from '../../model/model';
+import { APP_SETTINGS } from '../../app.settings';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class BoosterService {
+export class BoosterService extends HttpDefaultService {
+  constructor() {
+    super();
+  }
 
-  constructor() { }
+  retriveCreatureDeck(id: string): Observable<CardBooster[]> {
+    return this.getCreaturesDeckRecursively(id);
+  }
+
+  private getCreaturesDeckRecursively(
+    id: string,
+    creaturesDeck: CardBooster[] = []
+  ): Observable<CardBooster[]> {
+    this.loading.next(true);
+    return this.http
+      .get<BoosterPayload>(`${APP_SETTINGS.MTG_API}/v1/sets/${id}/booster`)
+      .pipe(
+        map(({ cards }) => cards.filter((c) => c.types.includes('creature'))),
+        tap((cards) => creaturesDeck.concat(cards)),
+        switchMap(() =>
+          creaturesDeck.length >= 30
+            ? of(creaturesDeck.slice(0, 30))
+            : this.getCreaturesDeckRecursively(id, creaturesDeck)
+        ),
+        filter(() => creaturesDeck.length === 30),
+        tap(() => this.loading.next(false))
+      );
+  }
 }
