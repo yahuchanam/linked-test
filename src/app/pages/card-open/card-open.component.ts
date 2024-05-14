8550839900080087c50addc9e697bb23441482e7
Marcus Borges <yahuchanam@gmail.com>
2024-05-14 09:50:22 -0300

    feat: add card open pages and routing

---
+++ src/app/pages/card-open/card-open.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-open',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-open.component.html',
+  styleUrl: './card-open.component.scss'
+})
+export class CardOpenComponent {
+
+}
